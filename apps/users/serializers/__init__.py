70a7909ef63205b7b621b8b4f3f618f05d461040
portfolio <adrianoopereiira@hotmail.com>
2025-11-30 09:29:44 -0300

    feat(users): implemes UserSimpleSerializer to return simples data of user

--- apps/users/serializers/__init__.py
+++ apps/users/serializers/__init__.py
@@ -1,4 +1,5 @@
 from .user_me_serializer import UserMeSerializer
 from .user_register_serializer import UserRegisterSerializer
+from .user_simple_serializer import UserSimpleSerializer
 
-__all__ = ["UserMeSerializer", "UserRegisterSerializer"]
+__all__ = ["UserMeSerializer", "UserRegisterSerializer", "UserSimpleSerializer"]
