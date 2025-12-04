91e40274ad37a38a6f6aa9622bac08517682452d
portfolio <adrianoopereiira@hotmail.com>
2025-12-03 22:13:19 -0300

    refactory(posts): update url to support nested routes

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework_nested import routers
 
-from apps.posts.viewset import PostListView
+from apps.comments.viewsets import CommentViewSet
+from apps.likes.viewsets import LikeViewSet
+from apps.posts.viewsets import PostViewSet
+
+router = routers.DefaultRouter()
+router.register(r"posts", PostViewSet, basename="posts")
+
+posts_router = routers.NestedDefaultRouter(router, r"posts", lookup="post")
+posts_router.register(r"likes", LikeViewSet, basename="post-likes")
+posts_router.register(r"comments", CommentViewSet, basename="post-comments")
+
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="posts"),
+    path("", include(router.urls)),
+    path("", include(posts_router.urls)),
 ]
